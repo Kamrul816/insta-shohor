c9666f7673349c71288c5754b5ab1d3bebf69366
Kamrul816 <freelancerkamrul816@gmail.com>
2022-03-26 05:58:58 +0600

    Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Honda Car World</h1>
+            <h3>Your best choice</h3>
+        </div>
+    );
+};
+
+export default Header;
